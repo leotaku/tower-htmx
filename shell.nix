43661027c946daf4da89ac21db526f32fa6485f3
Leo Gaskin <leo.gaskin@le0.gs>
2023-06-13 06:51:32 +0200

    Remove dependency on cargo-edit from shell.nix

--- shell.nix
+++ shell.nix
@@ -2,5 +2,5 @@
 with pkgs;
 stdenvNoCC.mkDerivation {
   name = "dev-shell";
-  buildInputs = [ cargo-edit rustup llvmPackages_latest.clang ];
+  buildInputs = [ rustup llvmPackages_latest.clang ];
 }
