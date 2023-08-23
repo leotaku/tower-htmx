d0d57db1f87e06aa3ff2e3a5e869dd07e47fac97
Leo Gaskin <leo.gaskin@le0.gs>
2023-08-23 14:01:09 +0200

    Add LiveReload layer to example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,14 +1,21 @@
+use axum::http::Request;
 use axum::Router;
 use tower_htmx::{SelectLayer, TemplateLayer};
 use tower_http::services::ServeDir;
+use tower_livereload::LiveReloadLayer;
 use tracing_subscriber::util::SubscriberInitExt;
+
+fn not_htmx_predicate<T>(req: &Request<T>) -> bool {
+    !req.headers().contains_key("hx-request")
+}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let app = Router::new()
         .nest_service("/", ServeDir::new("."))
         .layer(SelectLayer::new())
-        .layer(TemplateLayer::new());
+        .layer(TemplateLayer::new())
+        .layer(LiveReloadLayer::new().request_predicate(not_htmx_predicate));
 
     let addr = ([0, 0, 0, 0], 8080).into();
     eprintln!("listening on: http://{}/", addr);
