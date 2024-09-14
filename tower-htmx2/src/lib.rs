d9079f2ba6f6837611e5e0bbe31c4a523d96c73f
Leo Gaskin <leo.gaskin@le0.gs>
2024-09-14 16:47:41 +0200

    It now works

--- tower-htmx2/src/lib.rs
+++ tower-htmx2/src/lib.rs
@@ -1,16 +1,20 @@
 #![feature(impl_trait_in_assoc_type)]
 #![forbid(unused_unsafe)]
-//#![warn(clippy::all, missing_docs, nonstandard_style, future_incompatible)]
+#![warn(clippy::all, missing_docs, nonstandard_style, future_incompatible)]
 #![allow(clippy::type_complexity)]
+
+//! Foo
+
+mod future;
+mod sync;
 
 use std::future::{poll_fn, Future};
 use std::mem;
-use std::pin::pin;
 
-use bytes::{Buf, Bytes, BytesMut};
+use bytes::Bytes;
 use http::{Request, Response, Uri};
 use http_body_util::BodyExt;
-use lol_html::{element, HtmlRewriter, Settings};
+use sync::{rewrite, scan_tags};
 use tower::{Layer, Service};
 
 #[derive(Debug, Clone)]
@@ -42,174 +46,135 @@ impl<S> HtmxRewriteService<S> {
 impl<S, ReqBody, ResBody> Service<http::Request<ReqBody>> for HtmxRewriteService<S>
 where
     S: Service<http::Request<ReqBody>, Response = http::Response<ResBody>> + Clone,
-    S::Error: std::error::Error + Send + Sync + 'static,
     ReqBody: Default,
-    ResBody: http_body::Body,
+    ResBody: http_body::Body<Data = Bytes>,
 {
-    type Response = Response<http_body_util::Full<Bytes>>;
-    type Error = S::Error;
+    type Response = Response<http_body_util::Either<ResBody, http_body_util::Full<Bytes>>>;
+    type Error = Error<S::Error, ResBody::Error>;
     type Future = impl Future<Output = Result<Self::Response, Self::Error>>;
 
     fn poll_ready(
         &mut self,
         cx: &mut std::task::Context<'_>,
     ) -> std::task::Poll<Result<(), Self::Error>> {
-        self.0.poll_ready(cx)
+        self.0
+            .poll_ready(cx)
+            .map_err(|err| Error::Future(err.into()))
     }
 
     fn call(&mut self, req: http::Request<ReqBody>) -> Self::Future {
-        // if req.extensions().get::<RecursionProtector>().is_some() {
-        //     panic!("recursion detected")
-        // };
+        if req.headers().contains_key(http::header::CONTENT_LENGTH)
+            && req
+                .headers()
+                .get(http::header::CONTENT_TYPE)
+                .and_then(|it| it.to_str().ok())
+                .is_some_and(|it| it.starts_with("text/html"))
+        {
+            let fut = self.0.call(req);
+            return future::Either::left(async move {
+                let rsp = fut.await.map_err(Error::Future)?;
+                Ok(rsp.map(http_body_util::Either::Left))
+            });
+        };
 
         let uri = req.uri().clone();
-        dbg!(&uri);
-        let mut cloned = mem::replace(self, self.clone());
+        let recursion_level = match req.extensions().get::<RecursionProtector>() {
+            Some(RecursionProtector(level)) => *level,
+            None => 0,
+        };
+
         let fut = self.0.call(req);
+        let service = mem::replace(self, self.clone());
 
-        async move {
-            let cloned = &mut cloned;
-            let handle = tokio::runtime::Handle::current();
-            let res = fut.await?;
+        future::Either::right(Box::pin(async move {
+            let res = fut.await.map_err(Error::Future)?;
+            rewrite_call(&uri, res, service, recursion_level).await
+        }))
+    }
+}
 
-            let (parts, body) = res.into_parts();
-            let original = body.collect().await.ok().unwrap().to_bytes();
-            let mut new = BytesMut::new();
+#[derive(Debug)]
+pub enum Error<F, B> {
+    Future(F),
+    Body(B),
+    HTTP(http::Error),
+    Recursion,
+}
 
-            let mut rewriter = HtmlRewriter::new(
-                Settings {
-                    element_content_handlers: vec![
-                        // Rewrite insecure hyperlinks
-                        element!(r#"[hx-get][hx-trigger~="server"]"#, |el| {
-                            let attr = el.get_attribute("hx-get").unwrap();
-                            let req = Request::builder()
-                                .method(http::method::Method::GET)
-                                .uri(expand_uri(&uri, attr)?)
-                                .extension(RecursionProtector)
-                                .body(Default::default())?;
-
-                            let rsp = tokio::task::block_in_place(|| {
-                                handle.block_on(async {
-                                    poll_fn(|cx| cloned.poll_ready(cx)).await?;
-                                    let (parts, body) = cloned.call(req).await?.into_parts();
-                                    Ok::<_, Self::Error>(Response::from_parts(
-                                        parts,
-                                        body.collect().await,
-                                    ))
-                                })
-                            })?;
-
-                            let response_is_html = rsp
-                                .headers()
-                                .get(http::header::CONTENT_TYPE)
-                                .map(|it| it.as_ref().starts_with(b"text/html"))
-                                .unwrap_or(false);
-
-                            let ct = response_is_html
-                                .then_some(lol_html::html_content::ContentType::Html)
-                                .unwrap_or(lol_html::html_content::ContentType::Text);
-
-                            let body = rsp.into_body()?.to_bytes();
-                            let content = std::str::from_utf8(&body)?;
-
-                            match el.get_attribute("hx-swap").as_deref() {
-                                None | Some("innerHTML") => el.set_inner_content(content, ct),
-                                Some("outerHTML") => el.replace(content, ct),
-                                Some("afterbegin") => el.prepend(content, ct),
-                                Some("beforebegin") => el.before(content, ct),
-                                Some("beforeend") => el.append(content, ct),
-                                Some("afterend") => el.after(content, ct),
-                                Some("delete") => el.remove(),
-                                _ => (),
-                            }
-
-                            Ok(())
-                        }),
-                    ],
-                    ..Settings::default()
-                },
-                |chunk: &[u8]| new.extend_from_slice(chunk),
-            );
-
-            rewriter.write(&original).unwrap();
-
-            dbg!(&new);
-
-            Ok(http::Response::from_parts(
-                parts,
-                http_body_util::Full::new(new.into()),
-            ))
+impl<F, B> Error<F, B> {
+    pub fn to_html(self) -> String {
+        match self {
+            Error::Future(err) => "future error".to_owned(),
+            Error::Body(err) => "body error".to_owned(),
+            Error::HTTP(err) => "http error".to_owned(),
+            Error::Recursion => "recursion error".to_owned(),
         }
     }
 }
 
-#[derive(Debug, Clone)]
-struct RecursionProtector;
+async fn rewrite_call<S, ReqBody, ResBody>(
+    uri: &http::uri::Uri,
+    res: Response<ResBody>,
+    mut service: HtmxRewriteService<S>,
+    recursion_level: usize,
+) -> Result<
+    <HtmxRewriteService<S> as Service<http::Request<ReqBody>>>::Response,
+    Error<S::Error, ResBody::Error>,
+>
+where
+    S: Service<http::Request<ReqBody>, Response = http::Response<ResBody>> + Clone,
+    ReqBody: Default,
+    ResBody: http_body::Body<Data = Bytes>,
+{
+    if recursion_level > 10 {
+        return Err(Error::Recursion);
+    }
 
-fn expand_uri(uri: &http::uri::Uri, path: String) -> http::Result<http::uri::Uri> {
-    let mut p_and_q = if path.starts_with("/") {
-        path
+    let (mut parts, body) = res.into_parts();
+    let original = body.collect().await.map_err(Error::Body)?.to_bytes();
+    let hrefs = scan_tags(&original).unwrap();
+
+    let mut resps = Vec::new();
+    for href in hrefs {
+        poll_fn(|cx| service.poll_ready(cx)).await?;
+        let req = Request::builder()
+            .method(http::method::Method::GET)
+            .uri(expand_uri(&uri, &href).map_err(Error::HTTP)?)
+            .extension(RecursionProtector(recursion_level))
+            .body(ReqBody::default())
+            .unwrap();
+
+        let (parts, body) = service.call(req).await?.into_parts();
+        let body = match body {
+            http_body_util::Either::Left(left) => left.collect().await.map_err(Error::Body)?,
+            http_body_util::Either::Right(right) => right.collect().await.expect("fuck off"),
+        };
+
+        resps.push(Response::from_parts(parts, body));
+    }
+
+    let rewritten = rewrite::<ResBody::Data>(&original, resps).unwrap();
+
+    parts
+        .headers
+        .insert(http::header::CONTENT_LENGTH, rewritten.len().into());
+
+    Ok(http::Response::from_parts(
+        parts,
+        http_body_util::Either::Right(http_body_util::Full::new(rewritten)),
+    ))
+}
+
+fn expand_uri(uri: &http::uri::Uri, path: &str) -> http::Result<http::uri::Uri> {
+    let mut parts = uri.clone().into_parts();
+    parts.path_and_query = if path.starts_with("/") {
+        Some(path.try_into()?)
     } else {
-        format!("{}/{}", uri.path(), path)
+        Some(format!("{}/{}", uri.path(), path).try_into()?)
     };
 
-    if let Some(query) = uri.query() {
-        p_and_q.push_str(query);
-    }
-
-    Uri::builder()
-        .scheme(uri.scheme().cloned().unwrap_or(http::uri::Scheme::HTTP))
-        .authority(
-            uri.authority()
-                .cloned()
-                .unwrap_or("internal".try_into().unwrap())
-        )
-        .path_and_query(p_and_q)
-        .build()
+    Ok(Uri::from_parts(parts)?)
 }
 
 #[derive(Debug, Clone)]
-pub struct DummyLayer;
-
-impl DummyLayer {
-    pub fn new() -> Self {
-        DummyLayer
-    }
-}
-
-impl<S> Layer<S> for DummyLayer {
-    type Service = DummyService<S>;
-
-    fn layer(&self, inner: S) -> Self::Service {
-        DummyService::new(inner)
-    }
-}
-
-#[derive(Debug, Clone)]
-pub struct DummyService<S>(S);
-
-impl<S> DummyService<S> {
-    fn new(inner: S) -> Self {
-        DummyService(inner)
-    }
-}
-
-impl<S, ReqBody> Service<http::Request<ReqBody>> for DummyService<S>
-where
-    S: Service<http::Request<ReqBody>>,
-{
-    type Response = S::Response;
-    type Error = S::Error;
-    type Future = S::Future;
-
-    fn poll_ready(
-        &mut self,
-        cx: &mut std::task::Context<'_>,
-    ) -> std::task::Poll<Result<(), Self::Error>> {
-        todo!()
-    }
-
-    fn call(&mut self, req: http::Request<ReqBody>) -> Self::Future {
-        todo!()
-    }
-}
+struct RecursionProtector(usize);
