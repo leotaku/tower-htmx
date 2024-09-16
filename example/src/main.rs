c18ed1660167bc3d38ed688c9a5a15cb980688d3
Leo Gaskin <leo.gaskin@le0.gs>
2024-09-16 11:04:13 +0200

    Improve example

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,26 +1,22 @@
 use std::net::SocketAddr;
 
-use axum::error_handling::HandleErrorLayer;
-use axum::http::{Request, StatusCode};
-use axum::response::Html;
+use axum::http::StatusCode;
+use axum::response::{Html, IntoResponse};
 use axum::Router;
 use tokio::net::TcpListener;
-use tower::ServiceBuilder;
+use tower::service_fn;
 use tower_htmx::HtmxRewriteLayer;
 use tower_http::services::ServeDir;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let app = Router::new().nest_service("/", ServeDir::new("assets"));
-    let app = app.layer(
-        ServiceBuilder::new()
-            .layer(HandleErrorLayer::new(
-                |err: tower_htmx::Error<_, _>| async {
-                    (StatusCode::INTERNAL_SERVER_ERROR, Html(err.to_html()))
-                },
-            ))
-            .layer(HtmxRewriteLayer::new()),
-    );
+    let serve_dir = ServeDir::new("assets").not_found_service(service_fn(|_| async {
+        Ok((StatusCode::NOT_FOUND, Html("<pre>not found</pre>")).into_response())
+    }));
+
+    let app = Router::new()
+        .nest_service("/", serve_dir)
+        .layer(HtmxRewriteLayer::new().infallible());
 
     let addr: SocketAddr = ([0, 0, 0, 0], 8080).into();
     eprintln!("listening on: http://{}/", addr);
